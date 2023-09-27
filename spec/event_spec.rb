84b78c0771c34d0490d381e134bf7b6db7015cc3
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2023-09-27 14:55:04 -0400

    Add sorted_item_list

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -81,7 +81,7 @@ RSpec.describe Event do
       })
   end 
 
-  it 'can find overstocked items' do 
+    it 'can find overstocked items' do 
     @event.add_food_truck(@food_truck1)
     @event.add_food_truck(@food_truck2)  
     @event.add_food_truck(@food_truck3)
@@ -95,6 +95,6 @@ RSpec.describe Event do
     @event.add_food_truck(@food_truck2)  
     @event.add_food_truck(@food_truck3)
     expect(@event.sorted_item_list).to be_a Array
-    expect(@event.sorted_item_list).to eq(['Apple Pie (Slice)',"Banana Nice Cream",'Peach Pie (Slice)',"Peach-Raspberry Nice Cream"])
+    expect(@event.sorted_item_list).to eq(["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"])
   end
 end
